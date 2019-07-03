5376beb76d43bf26a494e9944b5a7d567e84d4cc
Mehmet Özçapkın <mehmeto@mac.com>
2019-07-03 16:16:24 +0300

    Test

--- Source/FolioReaderKit.swift
+++ Source/FolioReaderKit.swift
@@ -160,13 +160,19 @@ extension FolioReader {
     ///   - config: FolioReader configuration.
     ///   - shouldRemoveEpub: Boolean to remove the epub or not. Default true.
     ///   - animated: Pass true to animate the presentation; otherwise, pass false.
-    open func presentReader(parentViewController: UIViewController, withEpubPath epubPath: String, unzipPath: String? = nil, andConfig config: FolioReaderConfig, shouldRemoveEpub: Bool = true, animated:
-        Bool = true) {
+    open func presentReader(parentViewController: UIViewController,
+                            withEpubPath epubPath: String,
+                            unzipPath: String? = nil,
+                            andConfig config: FolioReaderConfig,
+                            shouldRemoveEpub: Bool = true, animated: Bool = true) {
+        
         let readerContainer = FolioReaderContainer(withConfig: config, folioReader: self, epubPath: epubPath, unzipPath: unzipPath, removeEpub: shouldRemoveEpub)
         self.readerContainer = readerContainer
         parentViewController.present(readerContainer, animated: animated, completion: nil)
         addObservers()
+        
     }
+    
 }
 
 // MARK: -  Getters and setters for stored values
